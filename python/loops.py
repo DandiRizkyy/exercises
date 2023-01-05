c2332b6cbfd8aba57a870980e3257981bba2ad1d
Dandi Rizky <dandirizkyy94@gmail.com>
2023-01-05 09:31:55 +0700

    Penggunaan Loops di Python

---
+++ python/loops.py
@@ -0,0 +1,29 @@
+#Print perkata
+names = "Tarmiji"
+
+for perkata in names:
+    print(perkata)
+
+#Print list satu nama
+names = ["Tarmiji"]
+
+for singlename in names:
+    print(singlename)
+
+#Print list semua nama
+names = ["Tarmiji", "Mike", "Loki"]
+
+for name in names:
+    print(name)
+
+
+#Cara Cepat Jika Range Number Lebih dari 1000++
+for i in range(6):
+    print(i)
+
+
+
+#Cara lama
+    #for i in [0, 1, 2, 3, 4, 5]:
+    #print(i)
+
