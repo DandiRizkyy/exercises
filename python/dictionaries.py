167587cb0db9dfe641ccee23ba3d4cf3f4a1a19a
Dandi Rizky <dandirizkyy94@gmail.com>
2023-01-05 09:58:01 +0700

    Dictionaries

---
+++ python/dictionaries.py
@@ -0,0 +1,6 @@
+#assign value to houses
+houses = {"Harry": "Gryfindor", "Ron": "Gryfindor"}
+
+#add / tambahkan data secara manual ke variable houses
+houses["Hermione"] = "Gryfindor"
+print(houses["Harry"])
