4646c720ef971e2e72f5582ede7a555d762305f2
Dandi Rizky <dandirizkyy94@gmail.com>
2023-01-05 11:34:25 +0700

    Belajar menggunakan fungsi dari file function

---
+++ python/kuadrat.py
@@ -0,0 +1,12 @@
+#bisa menggunakan ini
+from function import kuadrat
+
+for i in range(10):
+    print(f"Hasil dari kuadrat {i} adalah {kuadrat(i)}")
+
+
+#atau ini 
+import function
+
+for i in range(10):
+    print(f"Hasil dari kuadrat {i} adalah {function.kuadrat(i)}")
