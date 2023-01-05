286f15adf8be1bb605153e274dbf1b49b6b99b36
Dandi Rizky <dandirizkyy94@gmail.com>
2023-01-05 12:32:44 +0700

    Menggunakan Object Oriented Programming

---
+++ python/classes.py
@@ -0,0 +1,25 @@
+#membuat class bernama point
+print("OOP1")
+class Point():
+    def __init__(self, input1, input2, input3):
+        self.x = input1
+        self.y = input2
+        self.z = input3
+
+p = Point(5, 6, 7)
+print(p.x)
+print(p.y)
+print(p.z)
+print(" ")
+#Membuat class bernama Point2
+print("OOP2")
+class Point2():
+    def __init__(self, input1, input2, input3):
+        self.x = input1
+        self.y = input2
+        self.z = input3
+
+p = Point2(8, 9, 10)
+print(p.x)
+print(p.y)
+print(p.z)
